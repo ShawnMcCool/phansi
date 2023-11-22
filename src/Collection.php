16e0ed011c44a7f049f64c21bac590ce9de61c6f
Shawn McCool <shawn@mccool.email>
2023-11-22 12:16:13 +0100

    update Collection class

--- src/Collection.php
+++ src/Collection.php
@@ -1,51 +1,66 @@
-<?php namespace PhAnsi;
+<?php namespace Monolith\Collections;
 
+use Countable;
 use ArrayAccess;
 use ArrayIterator;
-use Countable;
 use IteratorAggregate;
 
-class Collection implements IteratorAggregate, Countable, ArrayAccess
+final class Collection implements IteratorAggregate, Countable, ArrayAccess
 {
-    protected array $items;
-
-    public function __construct(array $items = [])
-    {
-        $this->items = $items;
+    public function __construct(
+        private array $items = []
+    ) {
     }
 
-    public static function of(array $items): self
+    public function contains($value): bool
     {
-        return new static($items);
+        return in_array($value, $this->items);
     }
 
-    public static function empty(): self
+    public function containsMatch(callable $predicate): bool
     {
-        return new static;
-    }
+        $foundItem = $this->first($predicate);
 
-    public static function list(...$items): self
-    {
-        return new static($items);
+        return ! is_null($foundItem);
     }
 
     public function add($item): self
     {
         $items = $this->items;
         $items[] = $item;
-        return new static($items);
+        return new self($items);
     }
 
-    public function each(callable $f): void
+    public function each(callable $predicate): void
     {
         foreach ($this->items as $i) {
-            $f($i);
+            $predicate($i);
         }
     }
 
-    public function equals(Collection $that, callable $func = null): bool
+    public function index(int $index): mixed
     {
-        if (is_null($func)) {
+        if ( ! isset($this->items[$index])) {
+            return null;
+        }
+
+        return $this->items[$index];
+    }
+
+    public function indexFor(mixed $valueToFind): ?int
+    {
+        foreach ($this->items as $key => $value) {
+            if ($value === $valueToFind) {
+                return $key;
+            }
+        }
+
+        return null;
+    }
+
+    public function equals(Collection $that, callable $predicate = null): bool
+    {
+        if (is_null($predicate)) {
             return get_class($this) === get_class($that) && $this->items === $that->items;
         }
 
@@ -58,7 +73,7 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
         $two = $that->toArray();
 
         foreach (range(0, $this->count() - 1) as $i) {
-            if ( ! $func($one[0], $two[0])) {
+            if ( ! $predicate($one[0], $two[0])) {
                 return false;
             }
         }
@@ -71,14 +86,6 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
         return count($this->items);
     }
 
-    public function merge(Collection $that)
-    {
-        if (get_class($this) !== get_class($that)) {
-            throw new \Exception('CollectionTypeError::cannotMergeDifferentTypes');
-        }
-        return new static(array_merge($this->items, $that->items));
-    }
-    
     public function toArray(): array
     {
         return $this->copy()->items;
@@ -91,12 +98,22 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
 
     public function map(callable $f): self
     {
-        return new static(array_map($f, $this->items));
+        return new self(array_map($f, $this->items));
+    }
+
+    public function mapKeyValues(callable $f): self
+    {
+        return new self(array_map($f, array_keys($this->items), $this->items));
+    }
+
+    public function flatten(): self
+    {
+        return new self(array_merge(...array_map(fn($x) => $x, $this->items)));
     }
 
     public function flatMap(callable $f): self
     {
-        return new static(array_merge(...array_map($f, $this->items)));
+        return new self(array_merge(...array_map($f, $this->items)));
     }
 
     public function reduce(callable $f, $initial = null)
@@ -104,17 +121,27 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
         return array_reduce($this->items, $f, $initial);
     }
 
-    public function filter(?callable $f = null): self
+    public function filter(?callable $predicate = null): self
     {
-        return is_null($f)
-            ? new static(array_values(array_filter($this->items)))
-            : new static(array_values(array_filter($this->items, $f)));
+        return is_null($predicate)
+            ? new self(array_values(array_filter($this->items)))
+            : new self(array_values(array_filter($this->items, $predicate)));
     }
 
-    public function first(callable $f)
+    public function firstIndex(callable $predicate): ?int
+    {
+        foreach ($this->items as $index => $item) {
+            if ($predicate($item)) {
+                return $index;
+            }
+        }
+        return null;
+    }
+
+    public function first(callable $predicate)
     {
         foreach ($this->items as $item) {
-            if ($f($item)) {
+            if ($predicate($item)) {
                 return $item;
             }
         }
@@ -123,17 +150,37 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
 
     public function head()
     {
-        return reset($this->items) ?: null;
+        $value = reset($this->items);
+
+        if (false === $value) {
+            return null;
+        }
+        return $value;
     }
 
     public function tail(): self
     {
-        return new static(array_slice($this->items, 1));
+        return new self(array_slice($this->items, 1));
+    }
+
+    public function toDictionary(): Dictionary
+    {
+        return Dictionary::of(
+            $this->items
+        );
+    }
+
+    public function merge(Collection $that): self
+    {
+        if (get_class($this) !== get_class($that)) {
+            throw CollectionTypeError::cannotMergeDifferentTypes($this, $that);
+        }
+        return new self(array_merge($this->items, $that->items));
     }
 
     public function reverse(): self
     {
-        return new static(array_reverse($this->items));
+        return new self(array_reverse($this->items));
     }
 
     public function getIterator(): ArrayIterator
@@ -146,13 +193,20 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
         return empty($this->items);
     }
 
+    public function sort(?callable $f): self
+    {
+        $items = $this->items;
+        usort($items, $f);
+        return static::of($items);
+    }
+
     /**
      * Concatenates string items with a delimiter.
      *
      * @param string $delimiter
      * @return string
      */
-    public function implode($delimiter = ', '): string
+    public function implode(string $delimiter = ', '): string
     {
         return implode($delimiter, $this->items);
     }
@@ -169,7 +223,7 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
      * The return value will be casted to boolean if non-boolean was returned.
      * @since 5.0.0
      */
-    public function offsetExists($offset): bool
+    public function offsetExists(mixed $offset): bool
     {
         return array_key_exists($offset, $this->items);
     }
@@ -183,7 +237,7 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
      * @return mixed Can return all value types.
      * @since 5.0.0
      */
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         return $this->items[$offset];
     }
@@ -192,24 +246,57 @@ class Collection implements IteratorAggregate, Countable, ArrayAccess
      * Offset to set
      * This is not supported due to immutable nature.
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void
     {
-        throw new \Exception('CannotWriteToImmutableCollectionUsingArrayAccess');
+        throw new CannotWriteToImmutableCollectionUsingArrayAccess();
     }
 
     /**
      * Offset to unset
      * This is not supported due to immutable nature.
      */
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
-        throw new \Exception('CannotWriteToImmutableCollectionUsingArrayAccess');
+        throw new CannotWriteToImmutableCollectionUsingArrayAccess();
     }
 
-    public function zip(Collection $that)
+    public function zip(Collection $that): Dictionary
     {
-        return new static(
+        return Dictionary::of(
             array_map(null, $this->items, $that->items)
         );
+    }
+
+    public function unique(?callable $hashFunction = null)
+    {
+        if (is_null($hashFunction)) {
+            return new self(array_values(array_unique($this->items)));
+        }
+
+        $hashTable = MutableDictionary::empty();
+
+        $this->each(
+            function ($item) use ($hashTable, $hashFunction) {
+                $hash = $hashFunction($item);
+                $hashTable->add($hash, $item);
+            }
+        );
+
+        return $hashTable->toCollection();
+    }
+
+    public static function of(array $items): self
+    {
+        return new self($items);
+    }
+
+    public static function empty(): self
+    {
+        return new self;
+    }
+
+    public static function list(...$items): self
+    {
+        return new self($items);
     }
 }
